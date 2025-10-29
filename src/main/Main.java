a3118f33823efddb4c3aa188593553e8ea7e9901
Taisa Lima <taisalima300@gmail.com>
2025-10-29 07:41:55 -0300

    refactoring

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,18 @@
 package main;
 import java.io.*;
-import lexer.*;
-import parser.*;
+import lexer.Lexer;
+import parser.Parser;
+
+
 
 public class Main {
+
+    public static void Menu () {
+    System.out.println("---------Bem-vindo ao Compilador da linguagem Compini!--------");
+    System.out.println("\n");
+    System.out.println("Selecione a sua preferência       1. Testar o compilador manualmente         2. Explorar os testes disponíveis na nossa base de dados ");
+    }
     public static void main(String[] args) throws IOException{
-        Lexer lex=new Lexer();
-        Parser parse = new Parser(lex);
-        parse.program();
-        System.out.println("\n");
+        Menu();
     }
 }
